c6722c68f80bb2ea053ab569917c22dde185311d
M.Wildan Fadhilah <wildanfd11@gmail.com>
2023-05-03 10:12:27 +0700

    Adding model for table

---
+++ app/Models/TableModel.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class TableModel extends Model
+{
+    protected $DBGroup = 'default';
+    protected $table = 'datatables';
+    protected $primaryKey = 'id';
+    protected $useAutoIncrement = true;
+    protected $returnType = 'array';
+    protected $useSoftDeletes = false;
+    protected $protectFields = true;
+    protected $allowedFields = [
+        'nama',
+        'email',
+        'no_hp'
+    ];
+
+    // Dates
+    protected $useTimestamps = false;
+    protected $dateFormat = 'datetime';
+    protected $createdField = 'created_at';
+    protected $updatedField = 'updated_at';
+    protected $deletedField = 'deleted_at';
+
+    // Validation
+    protected $validationRules = [];
+    protected $validationMessages = [];
+    protected $skipValidation = false;
+    protected $cleanValidationRules = true;
+}
